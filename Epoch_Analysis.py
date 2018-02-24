a4f1db442b7b7e4c991ed50ce729c47ea186787f
Michael Gibbons-Physics <mag833@humboldt.edu>
2018-02-23 23:57:51 -0800

    Cut Gratings
    Added the functionality to choose if the grating will be cut, and if so where. For each of the three gratings.

--- Epoch_Analysis.py
+++ Epoch_Analysis.py
@@ -26,20 +26,61 @@ import Epoch_06_28_14_Normalization
 import Epoch_06_14_15_Normalization
 
 
-plt.figure(1)
-plt.title('Normalized Epoch 6/1/14, 6/12/14, 6/28/14 and 6/14/15  ')
-plt.xlabel(r'Observed Wavelength ($\AA$)')
-plt.ylabel('Flux (erg/s/cm^2/$\AA$)')
-plt.axhline(y=1,color='orange')
-plt.plot(Epoch_06_1_14_Normalization.Final_x_spectrum,Epoch_06_1_14_Normalization.Final_y_spectrum,zorder=0)
-plt.plot(Epoch_06_12_14_Normalization.Final_x_spectrum,Epoch_06_12_14_Normalization.Final_y_spectrum,zorder=0)
-plt.plot(Epoch_06_28_14_Normalization.Final_x_spectrum,Epoch_06_28_14_Normalization.Final_y_spectrum,zorder=0)
-plt.plot(Epoch_06_14_15_Normalization.Final_x_spectrum,Epoch_06_14_15_Normalization.Final_y_spectrum,zorder=0)
-Epoch_06_1_14 = mpatches.Patch(color='blue', label='Epoch_06_1_14')
-Epoch_06_12_14=mpatches.Patch(color='orange', label='Epoch_06_12_14')
-Epoch_06_28_14=mpatches.Patch(color='green', label='Epoch_06_28_14')
-Epoch_06_14_15=mpatches.Patch(color='red', label='Epoch_06_14_15')
-plt.legend(handles=[Epoch_06_1_14,Epoch_06_12_14,Epoch_06_28_14,Epoch_06_14_15])
-plt.axis([1400,1450,0,3])
-fig = matplotlib.pyplot.gcf()
-fig.set_size_inches(13.5, 10.5)
+Start_WL=900
+End_WL=1800
+Step_WL=25
+
+Current_WL=Start_WL
+for i in range((End_WL-Start_WL)//Step_WL):
+    plt.figure(1)
+    plt.title('Normalized Epoch 6/1/14, 6/12/14, 6/28/14 and 6/14/15  ')
+    plt.xlabel(r'Observed Wavelength ($\AA$)')
+    plt.ylabel('Flux (erg/s/cm^2/$\AA$)')
+    plt.axhline(y=1,color='orange')
+#    plt.plot(Epoch_06_1_14_Normalization.Final_x_spectrum,Epoch_06_1_14_Normalization.Final_y_spectrum,zorder=0,c='blue')
+#    plt.plot(Epoch_06_12_14_Normalization.Final_x_spectrum,Epoch_06_12_14_Normalization.Final_y_spectrum,zorder=0,c='orange')
+#    plt.plot(Epoch_06_28_14_Normalization.Final_x_spectrum,Epoch_06_28_14_Normalization.Final_y_spectrum,zorder=0,c='green')
+#    plt.plot(Epoch_06_14_15_Normalization.Final_x_spectrum,Epoch_06_14_15_Normalization.Final_y_spectrum,zorder=0,c='red')
+    plt.plot(Epoch_06_14_15_Normalization.TWL1,Epoch_06_14_15_Normalization.Normal_TFS1,zorder=0,c='blue')
+    plt.plot(Epoch_06_14_15_Normalization.TWL2,Epoch_06_14_15_Normalization.Normal_TFS2,zorder=0,c='orange')
+    plt.plot(Epoch_06_14_15_Normalization.TWL3,Epoch_06_14_15_Normalization.Normal_TFS3,zorder=0,c='red')
+    
+    plt.plot(Epoch_06_14_15_Normalization.Final_x_spectrum,Epoch_06_14_15_Normalization.Final_y_spectrum,zorder=0,c='green')
+#    Epoch_06_1_14 = mpatches.Patch(color='blue', label='Epoch_06_1_14')
+#    Epoch_06_12_14=mpatches.Patch(color='orange', label='Epoch_06_12_14')
+#    Epoch_06_28_14=mpatches.Patch(color='green', label='Epoch_06_28_14')
+#    Epoch_06_14_15=mpatches.Patch(color='red', label='Epoch_06_14_15')
+#    plt.legend(handles=[Epoch_06_1_14,Epoch_06_12_14,Epoch_06_28_14,Epoch_06_14_15])
+    plt.axis([Current_WL,Current_WL+Step_WL,0,3])
+    fig = matplotlib.pyplot.gcf()
+    fig.set_size_inches(13.5, 10.5)
+    
+    savefig('C:/Users/micha/Google Drive/Documents/Research/Plots/%s-%s'%(Current_WL,Current_WL+Step_WL))
+    Current_WL=Current_WL+Step_WL
+    
+#plt.figure(1)
+#plt.title('Normalized Epoch 6/1/14, 6/12/14, 6/28/14 and 6/14/15  ')
+#plt.xlabel(r'Observed Wavelength ($\AA$)')
+#plt.ylabel('Flux (erg/s/cm^2/$\AA$)')
+#plt.axhline(y=1,color='orange')
+#plt.plot(Epoch_06_1_14_Normalization.Final_x_spectrum,Epoch_06_1_14_Normalization.Final_y_spectrum,zorder=0,c='blue')
+#plt.plot(Epoch_06_12_14_Normalization.Final_x_spectrum,Epoch_06_12_14_Normalization.Final_y_spectrum,zorder=0,c='orange')
+#plt.plot(Epoch_06_28_14_Normalization.Final_x_spectrum,Epoch_06_28_14_Normalization.Final_y_spectrum,zorder=0,c='green')
+#
+#plt.plot(Epoch_06_14_15_Normalization.TWL1,Epoch_06_14_15_Normalization.Normal_TFS1,zorder=0,c='blue')
+#plt.plot(Epoch_06_14_15_Normalization.TWL2,Epoch_06_14_15_Normalization.Normal_TFS2,zorder=0,c='orange')
+#plt.plot(Epoch_06_14_15_Normalization.TWL3,Epoch_06_14_15_Normalization.Normal_TFS3,zorder=0,c='red')
+#
+#plt.plot(Epoch_06_14_15_Normalization.Final_x_spectrum,Epoch_06_14_15_Normalization.Final_y_spectrum,zorder=0,c='green')
+#
+#Epoch_06_1_14 = mpatches.Patch(color='blue', label='Epoch_06_1_14')
+#Epoch_06_12_14=mpatches.Patch(color='orange', label='Epoch_06_12_14')
+#Epoch_06_28_14=mpatches.Patch(color='green', label='Epoch_06_28_14')
+#Epoch_06_14_15=mpatches.Patch(color='red', label='Epoch_06_14_15')
+#plt.legend(handles=[Epoch_06_1_14,Epoch_06_12_14,Epoch_06_28_14,Epoch_06_14_15])
+#plt.axis([1000,1174,0,1.5])
+#fig = matplotlib.pyplot.gcf()
+#fig.set_size_inches(13.5, 10.5) 
+
+
+    
