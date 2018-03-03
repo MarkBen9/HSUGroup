67bbc7f0f5d892f70d18b98edf94f074842093f3
Michael Gibbons-Physics <mag833@humboldt.edu>
2018-03-02 19:11:16 -0800

    Revert "Comments and Cutting"
    This reverts commit 2ae7f75ce614918a95eb9e4c25413d502c2d92c9.

--- Epoch_06_1_14_Normalization.py
+++ Epoch_06_1_14_Normalization.py
@@ -16,9 +16,6 @@ from numpy import convolve
 import matplotlib.patches as mpatches
 from numpy import polyfit
 from numpy import polyval
-#------------------------------------------------------------------------------
-#Functions for Usage
-#these waters have frozen
 def badpix(list_name,xmin_indice,xmax_indice,replace_val):
     for i in range(xmin_indice,xmax_indice):
         list_name[i]=replace_val
@@ -57,7 +54,6 @@ def find_index_from_value(mylist, value):
     return find_index(mylist,closest_value(mylist,value))
 #------------------------------------------------------------------------------
 #Fits files Opening
-#Can't break the ice no more
 #File says that the date obs was 6/1/14 3:05:15
 Data1 = fits.open("../Data/lcbx01010_x1dsum.fits")
 #File says that the date obs was 6/1/14 3:37:30
@@ -68,16 +64,15 @@ Data3 = fits.open("../Data/lcbx01030_x1dsum.fits")
 #Data Extraction
 #headNa  = DataN[0].header headNb  = DataN[1].header where N is an int; was removed to save space 
 #Headers are used to give more information and details about what is in the Fits files
-#it's raining so hard now
+
 TbData1 = Data1[1].data
 Data1.close()
 TbData2 = Data2[1].data
 Data2.close()
 TbData3 = Data3[1].data
 Data3.close()
+
 #------------------------------------------------------------------------------
-#seperation from data stored into wavelength, flux measured, and error for each point
-#Can't seem to find a shore
 wavelength1=TbData1['wavelength']
 flux1=TbData1['flux']
 error1=TbData1['error']
@@ -90,8 +85,6 @@ wavelength3=TbData3['wavelength']
 flux3=TbData3['flux']
 error3=TbData3['error']
 #------------------------------------------------------------------------------
-#Combining
-#_____ __ _____
 #1162.9A - 1479.5A
 TWL1=concatenate((wavelength1[1],wavelength1[0]),axis=0)
 TF1=concatenate((flux1[1],flux1[0]),axis=0)
@@ -104,25 +97,19 @@ ER2=concatenate((error2[1],error2[0]),axis=0)
 TWL3=concatenate((wavelength3[1],wavelength3[0]),axis=0)
 TF3=concatenate((flux3[1],flux3[0]),axis=0)
 ER3=concatenate((error3[1],error3[0]),axis=0)
-#------------------------------------------------------------------------------
-#Swapping Varible names to make things clearer (for this code)
-#Where you swim for the other side
-TWL1,TWL2=TWL2,TWL1
-TF1,TF2=TF2,TF1
-ER1,ER2=ER2,ER1
+
+TWL1,TWL2=TWL2,TWL1 #SWAPPING VARIBLE NAMES SO IT MAKES SENSE
+TF1,TF2=TF2,TF1     #SWAPPING VARIBLE NAMES SO IT MAKES SENSE
+ER1,ER2=ER2,ER1     #SWAPPING VARIBLE NAMES SO IT MAKES SENSE
 #------------------------------------------------------------------------------
 #Observing points of interest
 #Search for flat regious with no absorption and emision points for normalization
-#_____ __ _____
 badpix(TF2,5180,5405,0)
 TFS1=smooth(TF1,20)
 TFS2=smooth(TF2,20)
 TFS3=smooth(TF3,20)
-#------------------------------------------------------------------------------
-#Creating a first order polynomial set
-#but you're swimming against the tide
-#Need to narrow section (MB)
-# For different epochs, input different ranges that represent the continuum. 
+###############################################################################
+###############################################################################
 Grating_1_polyfitpoints=[
                          make_polyfit_point(TWL1,TFS1,1110,1130),
                          make_polyfit_point(TWL1,TFS1,1150,1190)
@@ -136,9 +123,12 @@ Grating_3_polyfitpoints=[make_polyfit_point(TWL3,TFS3,1425,1460),
                          make_polyfit_point(TWL3,TFS3,1500,1525),
                          make_polyfit_point(TWL3,TFS3,1590,1605),
                          make_polyfit_point(TWL3,TFS3,1675,1720)]
-#------------------------------------------------------------------------------
-#Creates the First Order Polynomial
-#this all goes so much deeper
+
+# For different epochs, input different ranges that represent the continuum. 
+#That should be it.
+
+###############################################################################
+###############################################################################
 x_poly_1=[item[0]for item in Grating_1_polyfitpoints]
 y_poly_1=[item[1]for item in Grating_1_polyfitpoints]
 x_poly_2=[item[0]for item in Grating_2_polyfitpoints]
@@ -151,9 +141,7 @@ best_fit_poly_3=(polyfit(x_poly_3,y_poly_3,1))
 #poly_array_1=polyval(best_fit_poly_1, TWL1) #poly_array contains the polynomial EVALUATED by each x point in the grating.
 #poly_array_2=polyval(best_fit_poly_2, TWL2) #It is NOT used anywhere in the code but is useful if one wants to plot the slope.
 #poly_array_3=polyval(best_fit_poly_3, TWL3)
-#------------------------------------------------------------------------------
-#Normalization to First order, Second Order Will be placed in here
-#then some would like to say
+
 Normal_TFS1=[]#grating 1
 for i in range(len(TWL1)):
     Normal_TFS1.append(TFS1[i]/(best_fit_poly_1[0]*(TWL1[i])+ best_fit_poly_1[1]))
@@ -163,21 +151,15 @@ for i in range(len(TWL2)):
 Normal_TFS3=[]#grating 3
 for i in range(len(TWL3)):
     Normal_TFS3.append(TFS3[i]/(best_fit_poly_3[0]*(TWL3[i])+ best_fit_poly_3[1]))
-#------------------------------------------------------------------------------
-#Used to remove data sets of low value
-#Soaked in this world of water
+
 remove_small(Normal_TFS1,0.02)
 remove_small(Normal_TFS2,0.02)
 remove_small(Normal_TFS3,0.02)
-#------------------------------------------------------------------------------
-#Used to find seperations between points
-#Until it's washed away
+
 TWL1_STEPSIZE=TWL1[1]-TWL1[0]
 TWL2_STEPSIZE=TWL2[1]-TWL2[0]
 TWL3_STEPSIZE=TWL3[1]-TWL3[0]
-#------------------------------------------------------------------------------
-#Selects out largest seperation... Really trying to Automated Mich?
-#_____ __ _____
+
 if(TWL1_STEPSIZE<=TWL2_STEPSIZE):
     TWL_1_2_STEPSIZE=TWL2_STEPSIZE
 else:
@@ -187,27 +169,19 @@ if(TWL_1_2_STEPSIZE<=TWL3_STEPSIZE):
     TWL_1_2_3_STEPSIZE=TWL3_STEPSIZE
 else:
     TWL_1_2_3_STEPSIZE=TWL_1_2_STEPSIZE
-#------------------------------------------------------------------------------
-#Creates An array that will be used for interpolation
-#Where you swim for the other side
+
 TWL1_TWL2= arange(TWL1[0],TWL2[-1],TWL_1_2_STEPSIZE)
 TWL1_TWL2_TWL3= arange(TWL1[0],TWL3[-1],TWL_1_2_3_STEPSIZE)
-#------------------------------------------------------------------------------
-#this seems like a potential class?, So I will brake with DOUBLE --------------
-#------------------------------------------------------------------------------
-#Place value to cut the Gratings
-#_____ __ _____
+
 CUT_GRATINGS=True
 if( CUT_GRATINGS):# Enter the wavelength of the areas you would like to cut off, or "None"(no quotes)
     TWL1_Start_Cut=None#Will cut off all values before this 
     TWL1_End_Cut=None #Will cut off all values after this 
     TWL2_Start_Cut=1171.6
-    TWL2_End_Cut=1466.6
+    TWL2_End_Cut=1470    
     TWL3_Start_Cut=1390.7
     TWL3_End_Cut=None
-#------------------------------------------------------------------------------    
-#Creates indexing
-#But you're swimming against the tide
+    
     if not TWL1_Start_Cut==None:TWL1_Start_Cut_index=find_index_from_value(TWL1,TWL1_Start_Cut)
     else:
         TWL1_Start_Cut_index=None
@@ -226,9 +200,7 @@ if( CUT_GRATINGS):# Enter the wavelength of the areas you would like to cut off,
     if not TWL3_End_Cut==None:  TWL3_End_Cut_index=find_index_from_value(TWL3,TWL3_End_Cut)
     else:
         TWL3_End_Cut_index=None
-#------------------------------------------------------------------------------ 
-#interp using cut sections, 
-#_____ __ _____
+        
     Iflux1=interp(TWL1_TWL2,       TWL1[TWL1_Start_Cut_index:TWL1_End_Cut_index],Normal_TFS1[TWL1_Start_Cut_index:TWL1_End_Cut_index],left=0,right=0)
     Ierror1=interp(TWL1_TWL2,      TWL1[TWL1_Start_Cut_index:TWL1_End_Cut_index],ER1[TWL1_Start_Cut_index:TWL1_End_Cut_index],left=0,right=0)
     Iflux2=interp(TWL1_TWL2,       TWL2[TWL2_Start_Cut_index:TWL2_End_Cut_index],Normal_TFS2[TWL2_Start_Cut_index:TWL2_End_Cut_index],left=0,right=0)
@@ -275,72 +247,67 @@ if( CUT_GRATINGS):# Enter the wavelength of the areas you would like to cut off,
         for i in range(TWL3_End_Cut_index,len(TWL3)):
                 Normal_TFS3[i]=0
    
+            
 else:
+    
     Iflux1=interp(TWL1_TWL2,TWL1,Normal_TFS1,left=0,right=0)
     Ierror1=interp(TWL1_TWL2,TWL1,ER1,left=0,right=0)
     Iflux2=interp(TWL1_TWL2,TWL2,Normal_TFS2,left=0,right=0)
     Ierror2=interp(TWL1_TWL2,TWL2,ER2,left=0,right=0)
     Iflux3=interp(TWL1_TWL2_TWL3,TWL3,Normal_TFS3,left=0,right=0)
     Ierror3=interp(TWL1_TWL2_TWL3,TWL3,ER3,left=0,right=0)
-#------------------------------------------------------------------------------
-#this seems like a potential class?, So I will brake with DOUBLE ---------------
-#------------------------------------------------------------------------------
-#Removed, If Cut = False, "Uncomment" below, WITH NO INDENT!!
-#You can drown but you still survive
-    """
-    Iflux1=interp(TWL1_TWL2,TWL1,Normal_TFS1,left=0,right=0)
-    Ierror1=interp(TWL1_TWL2,TWL1,ER1,left=0,right=0)
-    Iflux2=interp(TWL1_TWL2,TWL2,Normal_TFS2,left=0,right=0)
-    Ierror2=interp(TWL1_TWL2,TWL2,ER2,left=0,right=0)
-    Iflux3=interp(TWL1_TWL2_TWL3,TWL3,Normal_TFS3,left=0,right=0)
-    Ierror3=interp(TWL1_TWL2_TWL3,TWL3,ER3,left=0,right=0)
-    """
-#------------------------------------------------------------------------------
-#Removing zero error points
-#No colour no feeling
+
+Iflux1=interp(TWL1_TWL2,TWL1,Normal_TFS1,left=0,right=0)
+Ierror1=interp(TWL1_TWL2,TWL1,ER1,left=0,right=0)
+Iflux2=interp(TWL1_TWL2,TWL2,Normal_TFS2,left=0,right=0)
+Ierror2=interp(TWL1_TWL2,TWL2,ER2,left=0,right=0)
+Iflux3=interp(TWL1_TWL2_TWL3,TWL3,Normal_TFS3,left=0,right=0)
+Ierror3=interp(TWL1_TWL2_TWL3,TWL3,ER3,left=0,right=0)
+
 TME1=remove_zero_error(Ierror1)
 TME2=remove_zero_error(Ierror2)
 TME3=remove_zero_error(Ierror3)
-#------------------------------------------------------------------------------
-#Another Major section that will have Double Braker----------------------------
-#------------------------------------------------------------------------------
-#changed for forloop, has same results using If statements
-#No time to sit around
+        
 Averaged_TFS1_TFS2=[]
 for i in range(0,len(TWL1_TWL2)):
-    if (Iflux2[i]==0 and Iflux1[i]==0).all():
+    if (Iflux1[i]==0 and Iflux2[i]==0).all():
             Averaged_TFS1_TFS2.append(0.0)
+    elif(Iflux1[i]==0 and Iflux2[i]!=0):
+        Averaged_TFS1_TFS2.append(Iflux2[i])
+    elif(Iflux1[i]!=0 and Iflux2[i]==0):
+        Averaged_TFS1_TFS2.append(Iflux1[i])
+        
+    elif(TME1[i]==1 and TME2[i]!=1):
+            Averaged_TFS1_TFS2.append(Iflux2[i])
+    elif(TME1[i]!=1 and TME2[i]==1):
+            Averaged_TFS1_TFS2.append(Iflux1[i])
+    elif(TME1[i]==1 and TME2[i]==1):
+            Averaged_TFS1_TFS2.append(0.0)
+        
     else:      
             weight=(1/TME1[i])
             weight2=(1/TME2[i])
             spec=Iflux1[i]
             spec2=Iflux2[i]
-            Averaged_TFS1_TFS2.append((weight*spec+weight2*spec2)/(weight+weight2))
+            Averaged_TFS1_TFS2.append((weight*spec+weight2*spec2)/(weight+weight2))  
+
+badpix(Averaged_TFS1_TFS2,32323,32550,0)
+Iflux_1_2=interp(TWL1_TWL2_TWL3,TWL1_TWL2,Averaged_TFS1_TFS2,left=0,right=0)
 
 Error_TFS1_TFS2=[]
 for i in range(len(TWL1_TWL2)):
-    if (Ierror1[i]!=0 and Ierror2[i]==0).all():
+    if (Ierror1[i]!=1 and Ierror2[i]==1).all():
         Error_TFS1_TFS2.append(Ierror1[i])
-    elif (Ierror1[i]==0 and Ierror2[i]!=0).all():
+    elif (Ierror1[i]==1 and Ierror2[i]!=1).all():
         Error_TFS1_TFS2.append(Ierror2[i])
-    elif (Ierror1[i]==0 and Ierror2[i]==0).all():
-        Error_TFS1_TFS2.append(0.0)            
-    else:#if neither of the error values are zero then take the weighted average of the error values.
+    elif (Ierror1[i]==1 and Ierror2[i]==1).all():
+        Error_TFS1_TFS2.append(1)            
+    else:#if neither of the error values are one then take the weighted average of the error values.
         Error_TFS1_TFS2.append(sqrt((Ierror1[i])**2+(Ierror2[i])**2))   
-#------------------------------------------------------------------------------
-#removing bad pixal and setting up fluz and error for next combination
-#No living no dying
-badpix(Averaged_TFS1_TFS2,32323,32550,0)
-Iflux_1_2=interp(TWL1_TWL2_TWL3,TWL1_TWL2,Averaged_TFS1_TFS2,left=0,right=0)
+
 Ierror_1_2=interp(TWL1_TWL2_TWL3,TWL1_TWL2,Error_TFS1_TFS2,left=0,right=0)
 TME_1_2=remove_zero_error(Ierror_1_2)
-#------------------------------------------------------------------------------
-#in order to temporarly solve this, I emitted a point from the array, BECAUSE REASONS
-#No one here to be found
-Iflux_1_2[46931]=0
-#------------------------------------------------------------------------------
-#finally combining for total spectrum
-#No entry no exit
+
 Averaged_TFS1_TFS2_TFS3=[]
 for i in range(0,len(TWL1_TWL2_TWL3)):  
     if(Iflux_1_2[i]==0 and Iflux3[i]==0):
@@ -349,16 +316,20 @@ for i in range(0,len(TWL1_TWL2_TWL3)):
         Averaged_TFS1_TFS2_TFS3.append(Iflux3[i])
     elif(Iflux_1_2[i]!=0 and Iflux3[i]==0):
         Averaged_TFS1_TFS2_TFS3.append(Iflux_1_2[i])
+        
+    elif(TME_1_2[i]==1 and TME3[i]!=1):
+            Averaged_TFS1_TFS2_TFS3.append(Iflux3[i])
+    elif(TME_1_2[i]!=1 and TME3[i]==1):
+            Averaged_TFS1_TFS2_TFS3.append(Iflux_1_2[i])
+    elif(TME_1_2[i]==1 and TME3[i]==1):
+            Averaged_TFS1_TFS2_TFS3.append(0.0)
+    
     else:
         weight=(1/TME_1_2[i])
         weight2=(1/TME3[i])
         spec=Iflux_1_2[i]
         spec2=Iflux3[i]
         Averaged_TFS1_TFS2_TFS3.append((weight*spec+weight2*spec2)/(weight+weight2))
-#------------------------------------------------------------------------------
-#This is the last of the Coding section, the rest is final array---------------
-#------------------------------------------------------------------------------
-#END
-#I like to be besides the sea
+
 Final_x_spectrum=TWL1_TWL2_TWL3
 Final_y_spectrum=Averaged_TFS1_TFS2_TFS3
