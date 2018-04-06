0e399c144596638d47656b195c8d37aa553a8fe9
MarkBen9 <mark.ben9@gmail.com>
2018-04-05 22:24:25 -0700

    Epoch_1_Absorption
    Added the first part in a series for normalization of different regions Begining with SI and CIV

--- Epoch_Analysis_from_FITS.py
+++ Epoch_Analysis_from_FITS.py
@@ -21,7 +21,7 @@ import matplotlib.pyplot as plt
 import matplotlib.style as style
 from astropy.io import fits
 style.use('dark_background')
-
+#------------------------------------------------------------------------------
 Epoch_06_1_14=fits.open('../Data/Epoch_06_1_14.fits')
 Epoch_06_1_14_DATA=Epoch_06_1_14[1].data
 Epoch_06_1_14.close()
@@ -37,12 +37,10 @@ Epoch_06_28_14.close()
 Epoch_06_14_15=fits.open('../Data/Epoch_06_14_15.fits')
 Epoch_06_14_15_DATA=Epoch_06_14_15[1].data
 Epoch_06_14_15.close()
-
-
+#------------------------------------------------------------------------------
 Epoch_06_1_14_WAVE=Epoch_06_1_14_DATA['wavelength']
 Epoch_06_1_14_FLUX=Epoch_06_1_14_DATA['flux']
 Epoch_06_1_14_ERROR=Epoch_06_1_14_DATA['error']
-
 
 Epoch_06_12_14_WAVE=Epoch_06_12_14_DATA['wavelength']
 Epoch_06_12_14_FLUX=Epoch_06_12_14_DATA['flux']
@@ -55,7 +53,7 @@ Epoch_06_28_14_ERROR=Epoch_06_28_14_DATA['error']
 Epoch_06_14_15_WAVE=Epoch_06_14_15_DATA['wavelength']
 Epoch_06_14_15_FLUX=Epoch_06_14_15_DATA['flux']
 Epoch_06_14_15_ERROR=Epoch_06_14_15_DATA['error']
-
+#------------------------------------------------------------------------------
 plt.plot(Epoch_06_1_14_WAVE,Epoch_06_1_14_FLUX,zorder=0,c='red',linewidth=1)
 plt.plot(Epoch_06_12_14_WAVE,Epoch_06_12_14_FLUX,zorder=0,c='green',linewidth=1)
 plt.plot(Epoch_06_28_14_WAVE,Epoch_06_28_14_FLUX,zorder=0,c='blue',linewidth=1)
